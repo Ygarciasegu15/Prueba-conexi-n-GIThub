19c3d2d6e7bd921a1efd0c433e91b086c8b1fcaf
Ygarciasegu15 <yesica.5garcia@outlook.com>
2023-10-30 19:48:41 -0500

    the app is working fine.

--- Prueba conexiòn GIThub/Program.cs
+++ Prueba conexiòn GIThub/Program.cs
@@ -1,5 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Esta es la prueba de github");
-Console.WriteLine("Esta linea la hizo Yesica");
+
+Console.WriteLine("Multiplique () x ()");
+Console.WriteLine("Ingrese el primer nùmero");
+var numberOne = int.Parse (Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo numero");
+var numbertwo = int.Parse (Console.ReadLine());
+
+var resultado = numberOne * numbertwo;
+
+Console.WriteLine("Resultado"+ resultado);
 

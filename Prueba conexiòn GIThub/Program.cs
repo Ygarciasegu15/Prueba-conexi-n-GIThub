fffcdb1ebb23934711f10bcb545a975c9a29f175
Ygarciasegu15 <yesica.5garcia@outlook.com>
2023-10-30 19:33:30 -0500

    Last line was removed.

--- Prueba conexiòn GIThub/Program.cs
+++ Prueba conexiòn GIThub/Program.cs
@@ -2,4 +2,4 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Esta es la prueba de github");
 Console.WriteLine("Esta linea la hizo Yesica");
-Console.WriteLine("rRevisa el Codigo en Visual Studio");
+

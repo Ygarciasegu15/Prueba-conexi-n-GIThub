236764573a7b73e9e8ee66c6ca93fad71cf2f15f
Ygarciasegu15 <yesica.5garcia@outlook.com>
2023-10-30 19:28:14 -0500

    an script was added

--- Prueba conexiòn GIThub/Program.cs
+++ Prueba conexiòn GIThub/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Esta es la prueba de github");
+Console.WriteLine("Esta linea la hizo Yesica");
